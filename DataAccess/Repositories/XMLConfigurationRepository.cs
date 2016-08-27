08eb09654af65cc3fb3d4dfbde7e51a24d89f68f
Øystein Aalstad Jonasson <tarkeel@gmail.com>
2016-08-27 22:11:36 +0200

    XML Start
    Basics of XML Repository implemented

---
+++ DataAccess/Repositories/XMLConfigurationRepository.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml.Linq;
+
+namespace DataAccess.Repositories
+{
+    sealed class XMLConfigurationRepository : AbstractConfigurationRepository
+    {
+        #region Attributes and Constructor
+        private XMLRepositoryFactory factory;
+        private XDocument document;
+        private Dictionary<string, string> settings;
+        internal XMLConfigurationRepository(XMLRepositoryFactory _factory)
+        {
+            factory = _factory;
+            document = _factory.Document;
+            settings = new Dictionary<string, string>();
+        }
+        #endregion
+        #region Interface Overrides
+        public override string GetValue(string Setting)
+        {
+            string value;
+            //Check if the setting is stored
+            if (settings.TryGetValue(Setting, out value)) { return value; }
+            //If not, find the correct element in the file
+            XElement element = FindElementByKey(Setting);
+            //Send the element to parsing, which will return null if it's blank.
+            return ParseSetting(element);
+        }
+        public override void SetValue(string Setting, string Value, bool persist = true)
+        {
+            //Find the element
+            XElement element = FindElementByKey(Setting);
+            if (element == null)
+            {
+                //Setting isn't stored, so create it
+                element = new XElement("Setting",
+                    new XAttribute("Key", Setting),
+                    new XAttribute("Value", Value));
+                document.Root.Element("Config").Add(element);
+            }
+            else
+            {
+                //Update the element
+                XAttribute _value = element.Attribute("Value");
+                if (_value == null) { element.Add(new XAttribute("Key", Value)); }
+                else { _value.Value = Value; }
+                //Update dictionary
+                settings[Setting] = Value;
+            }
+            if (persist) { factory.Save(); }
+        }
+        public override void ClearSetting(string Setting, bool persist = true)
+        {
+            //Find the element
+            XElement element = FindElementByKey(Setting);
+            //Remove from document
+            if (element != null) { element.Remove(); }
+            //Remove from dictionary
+            settings.Remove(Setting);
+            if (persist) { factory.Save(); }
+        }
+        #endregion
+        #region XML Handling
+        internal XElement FindElementByKey(string key)
+        {
+            return (from XElement in document.Root.Element("Config").Elements()
+                    where XElement.Attribute("Key").Value.Equals(key)
+                    select XElement).FirstOrDefault();
+        }
+        internal string ParseSetting(XElement element)
+        {
+            //Check input
+            if (element == null) { return null; }
+            //Setting attribute
+            string setting = "";
+            XAttribute _setting = element.Attribute("Key");
+            if (_setting != null) { setting = _setting.Value; }
+            //Value attribute
+            string value = "";
+            XAttribute _value = element.Attribute("Value");
+            if (_value != null) { value = _value.Value; }
+            //Store and return
+            settings.Add(setting, value);
+            return value;
+        }
+        #endregion
+    }
+}
---
+++ DataAccess/Repositories/XMLRepositoryFactory.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml.Linq;
+using DataAccess.Types;
+
+namespace DataAccess.Repositories
+{
+    public sealed class XMLRepositoryFactory : AbstractRepositoryFactory
+    {
+        #region Internal attributes
+        internal XDocument Document { get; private set; }
+        private string filename;
+        #endregion
+        #region Singleton
+        //We apply the Singleton pattern to ensure that only one repository is in use.
+        private static readonly Lazy<XMLRepositoryFactory> instance = new Lazy<XMLRepositoryFactory>(() =>
+            new XMLRepositoryFactory(@"C:\Projects\CardRepository.xml"));
+        public static AbstractRepositoryFactory Instance
+        {
+            get { return instance.Value; }
+        }
+        private XMLRepositoryFactory(string _filename)
+        {
+            filename = _filename;
+            try
+            {
+                //Read in the file
+                Document = XDocument.Load(filename);
+                //Check the version stored, update as needed
+                Int32 xmlVersion = Convert.ToInt32(Document.Root.Attribute("version").Value);
+                if (xmlVersion != currentVersion)
+                {
+                    if (! updateVersion())
+                    {
+                        //TODO: Throw new invalid file exception
+                    }
+                }
+            }
+            //System.IO.DirectoryNotFoundException
+            catch (System.IO.FileNotFoundException ex)
+            {
+                //File not found, so create it.
+                Document = new XDocument(
+                    new XElement("Repository",
+                        new XAttribute("version", currentVersion), //Store which version of the XML datafile we're creating
+                        new XElement("Config")
+                        //TODO: Add new elements for other top nodes
+                    )
+                );
+                Save();
+            }
+            //Initialize Configuration
+            configurationRepository = new XMLConfigurationRepository(this);
+        }
+        #endregion
+        #region Versioning
+        private Int32 currentVersion = 0;
+        /* Version History:
+         *
+         */
+        /// <summary>
+        /// Update the XML Document to the latest version.
+        /// </summary>
+        /// <returns></returns>
+        private bool updateVersion()
+        {
+            Int32 oldVersion = Convert.ToInt32(Document.Root.Attribute("version").Value);
+            switch (oldVersion)
+            {
+                case 0:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        #endregion
+        #region Repository Delegates
+        private XMLConfigurationRepository configurationRepository;
+        public override AbstractConfigurationRepository ConfigurationRepository
+        {
+            get { return configurationRepository; }
+        }
+        #endregion
+        internal void Save()
+        {
+            Document.Save(filename);
+        }
+        public override void Persist()
+        {
+            Save();
+        }
+    }
+}
